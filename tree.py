de0877e67521665090a2fbfc781544c507b65652
sl <slucius@mail.ustc.edu.cn>
2018-09-27 10:51:34 +0800

    BFS & DFS

--- tree.py
+++ tree.py
@@ -39,14 +39,14 @@ def dfs1(graph, start):
 # print(dfs(graph, 'A'))
 
 
-def dfs2(graph, start, visited = None):
-    '''recursive.'''
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    for next in graph[start]-visited:
-        dfs2(graph, next, visited)
-    return visited
+# def dfs2(graph, start, visited = None):
+#     '''recursive.'''
+#     if visited is None:
+#         visited = set()
+#     visited.add(start)
+#     for next in graph[start]-visited:
+#         dfs2(graph, next, visited)
+#     return visited
 
 # print(dfs2(graph, 'C'))
 
@@ -75,4 +75,93 @@ def bfs(graph, start):
             queue.extend(graph[vertex]-visited)
     return visited
 
-print(bfs(graph, 'A'))
+# print(bfs(graph, 'A'))
+
+
+
+
+####################################################################################################
+
+
+class Graph(object):
+    def __init__(self, nodes, sides):
+        self.sequence = {}
+        self.tmp = []
+
+        for node in nodes:
+            for side in sides:
+                if node == side[0]:
+                    self.tmp.append(side[1])
+                elif node == side[1]:
+                    self.tmp.append(side[0])
+            self.sequence[node] = self.tmp
+
+            self.tmp = []
+
+
+    def dfs(self, node0):
+        stack = []
+        order = []
+        stack.append(node0)
+
+        while stack:
+            v = stack.pop()
+            order.append(v)
+
+            for w in self.sequence[v]:
+                if w not in stack and w not in order:
+                    stack.append(w)
+        return order
+
+
+    # def dfs_recursive(self, node0, visited = None):
+    #     if visited == None:
+    #         visited = []
+    #
+    #     visited.append(node0)
+    #     for w in self.sequence[]
+
+    def dfs2(self, node0, visited=None):
+        '''recursive.'''
+        if visited is None:
+            visited = []
+        visited.append(node0)
+        for w in self.sequence[node0]:
+            if w not in visited:
+                self.dfs2(w, visited)
+        return visited
+
+
+
+
+    def bfs(self, node0):
+        queue = []
+        order = []
+        queue.append(node0)
+        order.append(node0)
+
+        while queue:
+            v =queue.pop(0)
+
+            for w in self.sequence[v]:
+                if w not in order:
+                    queue.append(w)
+                    order.append(w)
+        return order
+
+
+
+
+
+
+
+
+nodes = [i+1 for i in range(8)]
+
+sides = [(1, 2),(1, 3),(2, 4),(2, 5),(4, 8),(5, 8),(3, 6),(3, 7),(6, 7)]
+
+G = Graph(nodes, sides)
+
+print(G.dfs(2))
+print(G.dfs2(2))
+# print(G.sequence)
