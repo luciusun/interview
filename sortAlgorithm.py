f79cc2365398ec2a1704efbabb69a9253545c744
sl <slucius@mail.ustc.edu.cn>
2018-09-27 15:35:35 +0800

    经典排序算法

--- sortAlgorithm.py
+++ sortAlgorithm.py
@@ -8,34 +8,8 @@ document note: some排序算法
 __author__ = 'slucius'
 
 
-# def quicksort(alist, left, right):
-#     if left < right:
-#         pivotLocation = partition(alist, left, right)
-#         quicksort(alist, left, pivotLocation-1)
-#         quicksort(alist, pivotLocation+1, right)
-#
-# def partition(alist, left, right):
-#     mid = (left+right) // 2
-#     #swap
-#     pivot = alist[mid]
-#     alist[mid] = alist[right]
-#     alist[right] = pivot
-#
-#     #set boundary
-#     boundary = left
-#     for i in range(left, right):
-#         if alist[i] < pivot:
-#             swap(alist, i, boundary)
-#             boundary += 1
-#     swap(alist, right ,boundary)
-#
-#     return boundary
-#
-# def swap(alist, i, j):
-#     alist[i], alist[j] = alist[j], alist[i]
 
-
-
+# 快速排序,O(nlogn)
 def quicksort(alist, left, right):
     if left < right:
         pivotLocation = partition(alist, left, right)
@@ -61,7 +35,45 @@ def swap(alist, i, j):
     alist[i], alist[j] = alist[j], alist[i]
 
 
+# alist = [2, 9, 10, 4, 3, 6]
+# quicksort(alist, 0, len(alist)-1)
+# print(alist)
+
+
+
+
+#归并排序,O(nlogn),采用分治法,且各层分治递归可以同时进行。
+
+def mergeSort(alist):
+    if len(alist) <= 1:
+        return alist
+
+    mid = len(alist) // 2
+    left = mergeSort(alist[:mid])
+    right = mergeSort(alist[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    """合并两个已排序好的列表，产生一个新的已排序好的列表"""
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+    return result
+
 alist = [2, 9, 10, 4, 3, 6]
-quicksort(alist, 0, len(alist)-1)
-print(alist)
+print(mergeSort(alist))
+
+
 
